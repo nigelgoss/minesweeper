30e0b3108a6ff417a50d8d5bdda51832ebfe18c2
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-08-03 21:12:54 +0100

    Update screenshots.js

--- __tests__/screenshots.js
+++ __tests__/screenshots.js
@@ -1,18 +1,15 @@
 const timeout = process.env.SLOWMO ? 30000 : 10000;
 
 beforeAll(async () => {
-    await page.goto(URL, {waitUntil: 'domcontentloaded'});
+  await page.goto(URL, {waitUntil: 'domcontentloaded'});
 });
 
 describe('Test header and title of the page', () => {
+
     test('Title of the page', async () => {
         const title = await page.title();
-        await page.screenshot({path: 'homepage.png'});   
+        await page.screenshot({path: 'homepage.png'});
         expect(title).toBe('E2E Puppeteer Testing');
-        
-    }, timeout);
-    
-})();
-    
+    }, timeout);  
     
 });
