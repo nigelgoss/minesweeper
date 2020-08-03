bdd7f49a52fd1f053429bb4f51a423bd114d8f6d
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-08-03 21:08:16 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-    preset: "jest-puppeteer",
-    globals: {
-      URL: "http://localhost:8080"
-    },
-    testMatch: [
-      "**/test/**/*.test.js"
-    ],
-    verbose: true
+  preset: "jest-puppeteer",
+  globals: {
+    URL: "http://apps.nigelgoss.co.uk/minesweeper/"
+  },
+  verbose: true
 }
